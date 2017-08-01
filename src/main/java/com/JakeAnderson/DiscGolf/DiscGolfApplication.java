b230cf8dfe30dddbb83478ac73cfbca5588d73c7
Jake Anderson <anderson.jake.c@gmail.com>
2017-08-01 11:48:46 -0500

    Completed user login.

---
+++ src/main/java/com/JakeAnderson/DiscGolf/DiscGolfApplication.java
@@ -0,0 +1,12 @@
+package com.JakeAnderson.DiscGolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscGolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscGolfApplication.class, args);
+	}
+}
---
+++ src/main/java/com/JakeAnderson/DiscGolf/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.JakeAnderson.DiscGolf.controllers;
+
+import com.JakeAnderson.DiscGolf.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new User());
+        model.addAttribute("title", "Register");
+        return "user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid User user,
+                      Errors errors) {
+
+        model.addAttribute(user);
+
+        if (!errors.hasErrors()) {
+            return "user/index";
+        }
+
+        return "user/add";
+
+    }
+}
